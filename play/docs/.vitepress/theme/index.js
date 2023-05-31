e43b60f7e31568efa5f6ae49080f534bc595fe91
xdd2123 <zhudongdong050@chinasoftinc.com>
2023-05-31 17:19:44 +0800

    fix: 修复文档打包报错的问题

--- play/docs/.vitepress/theme/index.js
+++ play/docs/.vitepress/theme/index.js
@@ -1,10 +1,10 @@
 import DefaultTheme from "vitepress/theme";
-import xddUI from 'xdd-ui'
 export default {
   ...DefaultTheme,
   enhanceApp: async ({ app }) => {
-    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
-    // custom router. `siteData`` is a `ref`` of current site-level metadata.
-    app.use(xddUI)
+    if (!import.meta.env.SSR) {
+      const plugin = await import('xdd-ui')
+      app.use(plugin.default)
+    }
   },
 }
--- play/docs/components/button/index.md
+++ play/docs/components/button/index.md
@@ -1,13 +1,14 @@
 ### 按钮
-
-<xdd-button>默认按钮</xdd-button>
-<xdd-button type="success">怎么说</xdd-button>
+<ClientOnly>
+  <xdd-button>默认按钮</xdd-button>
+  <xdd-button type="success">怎么说</xdd-button>
+</ClientOnly>
 
 ::: details 显示代码
 
 ```vue
-<xdd-button>默认按钮</xdd-button>
-<xdd-button type="success">成功按钮</xdd-button>
+  <xdd-button>默认按钮</xdd-button>
+  <xdd-button type="success">成功按钮</xdd-button>
 ```
 
 :::
